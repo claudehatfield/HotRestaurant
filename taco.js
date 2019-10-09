b0b3cd37d6073e956b2d61bbf237d1b8ee0ad7f7
Angelica Zamer <angelica.zamer@gmail.com>
2019-10-09 14:03:11 -0500

    added app.use to setup Express app

--- taco.js
+++ taco.js
@@ -5,6 +5,8 @@ var path = require("path");
 //Set up the Express app
 var app = express();
 var PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json());
 
 var table = [
     {
